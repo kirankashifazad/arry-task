98829fe8bfb4c9b4a609f5938788e3756e24e909
kiran kashif azad <kirankashifazad@gmail.com>
2024-03-08 23:34:43 +0500

    work is done

---
+++ app.js
@@ -0,0 +1,227 @@
+(no1)
+
+var futureStudentNames = [];
+
+(2)
+var futureStudentNames = {
+    names: []
+  };
+
+  (3)
+
+  var stringsArr = ["ali", "soha", "arib", "xyz"];
+ (4) 
+
+var numArr = [23,100,45,78,890];
+
+(5)
+var bulianArr=[true,false]
+(6)
+
+var mixArr=[1,2,,3,"ali" ,"rania" ,true,false]
+
+
+(7)
+var arr=[ "SSC", "HSC", "BCS", "BS" ,'BCOM' ,'MS',"M.Phil","PHD"]
+  document.write("<h1>", arr[0]+"<br> " +arr[1]+"<br> "+ arr[2]+"<br> "+ arr[3] +"<br> "+ arr[4]+"<br> "+ arr[5]+" <br>"+ arr[6]+"<br> "+ arr[7] ,"</h1>")
+  
+
+//   (8)jlkjkjlj
+
+  
+ 
+var studentNames = [];
+studentNames.push("kiran","ali" ,"sara")
+var studentScores = [];
+studentScores.push(430,470,480)
+
+var tt_marks =500;
+document.write("<h1> student Name:"   + studentNames+" <br>" + "studentScore:" + studentScores +" <br>" +studentScores[2]/tt_marks *100 + "%" +"<br>" + "</h1> ");
+  
+// (9)
+
+var arr=["<h1>", "blue","green","red","yellow","</h1>"]
+document.write ( arr)
+var arr=[ "blue","green","red","yellow"]
+
+arr.unshift("orang")
+document.write(arr,"<br>")
+
+var arr=["blue","green","red","yellow"]
+
+arr.push("orange")
+document.write(arr,"<br>")
+
+var arr=[ "blue","green","red","yellow"]
+
+arr.unshift("orang","black")
+document.write(arr,"<br>")
+
+
+var arr=["blue","green","red","yellow"]
+
+arr.shift()
+document.write(arr,"<br>")
+
+var arr=["blue","green","red","yellow"]
+
+arr.pop()
+document.write(arr,"<br>")
+
+var arr=["blue","green","red","yellow"]
+
+arr.splice(2,1)
+document.write(arr,"<br>")
+
+var arr=["blue","green","red","yellow"]
+
+arr.splice(2,1 ,"tahira")
+document.write(arr,"<br>")
+
+ var a =("<h1> sort function</h1>")
+document.write(a)
+
+var arr=[ 304,654,122,305,786]
+
+document.write(arr + "<br>")
+
+arr.sort();
+document.write(arr +"<br>")
+
+
+var a=["<h1> slice method </h1>"+"<br>"]
+document.write(a)
+
+var arr =[ "karachi","lahore","islamabad","queta","multan"]
+document.write (arr,"<br>")
+var b=arr.slice (2,4)
+document.write(b +"<br>")
+
+var a=["<h1> concat </h1>"+"<br>"]
+document.write(a)
+
+
+var arr = ["This", "is", "my", "cat"];
+document.write(arr,"<br>")
+var result = arr.join(" ");
+document.write(result);
+    
+
+
+  
+
+var a=["<h1>Devises</h1>"+"<br>"]
+document.write(a)
+
+
+
+
+var arr=[ "monitor" ,"printer","keyBoard","mouse"]
+document.write(arr ,"<br>")
+var b=arr.shift ()
+document.write(b +"<br>")
+var c=arr.shift ()
+document.write(c +"<br>")
+var d=arr.shift ()
+document.write(d +"<br>")
+var e=arr.shift ()
+document.write(e +"<br> <br> <br>")
+
+
+var arr=[ "monitor" ,"printer","keyBoard","mouse"]
+document.write(arr ,"<br>")
+var b=arr.pop ()
+document.write(b +"<br>")
+var c=arr.pop ()
+document.write(c +"<br>")
+var d=arr.pop ()
+document.write(d +"<br>")
+var e=arr.pop ()
+document.write(e +"<br>")
+
+
+
+
+
+
+// /  assigment 9 task//////
+
+ var arr = ["laraib","usman","haider","raza","abid","noor","<br>"]
+ document.write(arr)
+ var arr2 = arr.slice(0,4,)
+ document.write(arr2,"<br>","<br>")
+
+
+var arr = ["laraib","usman","haider","raza","abid","noor","<br>"]
+document.write(arr)
+arr.splice(1,4)
+document.write(arr2,"<br>","<br>")
+
+
+var arr = ["laraib","usman","haider","raza","abid","noor","<br>"]
+document.write(arr)
+arr.shift()
+document.write(arr,"<br>","<br>")
+
+var arr = ["laraib","usman","haider","raza","abid","noor","<br>"]
+document.write(arr)
+arr.unshift("orange")
+document.write(arr,"<br>","<br>")
+
+var arr = ["laraib","usman","haider","raza","abid","noor"]
+document.write(arr,"<br>")
+var arr1 = ["laraib","usman","haider","raza","abid","noor"]
+arr.pop()
+document.write(arr,"<br>","<br>")
+
+
+var arr = ["laraib","usman","haider","raza","abid","noor"]
+document.write(arr,"<br>")
+var arr1 = ["laraib","usman","haider","raza","abid","noor"]
+arr.push("kiran")
+document.write(arr,"<br>","<br>")
+
+
+
+var arr = ["laraib","usman","haider","raza","abid","noor"]
+document.write(arr,"<br>")
+var arr = ["laraib","usman","haider","raza","abid","noor"]
+var arr2 =arr.slice(1,3)
+
+document.write(arr2,"<br>","<br>")
+
+
+var arr = ["laraib","usman","haider","raza","abid","noor"]
+document.write(arr,"<br>")
+var arr = ["laraib","usman","haider","raza","abid","noor"]
+var arr2 =arr.splice(1,1)
+
+document.write(arr2,"<br>","<br>")
+
+
+var arr=[ "monitor" ,"printer","keyBoard","mouse"]
+document.write(arr ,"<br>")
+var b=arr.shift ()
+document.write(b +"<br>")
+var c=arr.shift ()
+document.write(c +"<br>")
+var d=arr.shift ()
+document.write(d +"<br>")
+var e=arr.shift ()
+document.write(e +"<br> <br> <br>")
+
+
+var arr=[ "monitor" ,"printer","keyBoard","mouse"]
+document.write(arr ,"<br>")
+var b=arr.pop ()
+document.write(b +"<br>")
+var c=arr.pop ()
+document.write(c +"<br>")
+var d=arr.pop ()
+document.write(d +"<br>")
+var e=arr.pop ()
+document.write(e +"<br>")
+
+
+
+// for loop//////
